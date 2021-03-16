0aee1f68d9316a4a8647277eeb4eb8a63cb7b4f9
Ubuntu <ubuntu@ip-172-31-47-238.eu-west-1.compute.internal>
2021-03-16 11:54:09 +0000

    cicd

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public String WELCOME_MESSAGE;
+
+    public WelcomeController(@Value("${welcome.message}") String WELCOME_MESSAGE) {
+        this.WELCOME_MESSAGE = WELCOME_MESSAGE;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return WELCOME_MESSAGE;
     }
 }
+
+
+
+
